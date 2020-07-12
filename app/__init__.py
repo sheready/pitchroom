641c90835b68159e71fc7ace3b128220239c2df2
sheready <csstlw@gmail.com>
2020-07-12 21:02:51 +0300

    Add navbar

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask import Flask
+from .config import DevConfig
+from config import config_options
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+
+def create_app(config_name):
+    app = Flask(__name__,instance_relative_config = True)
+
+    app.config.from_object(DevConfig)
+    app.config.from_pyfile("config.py")
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+
+from app import views
