1cabd93ceefecf7e28924c9a03f84c1e18be0080
sheready <csstlw@gmail.com>
2020-07-18 09:54:38 +0300

    add bootstrap extension

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask import Flask
 from .config import DevConfig
-from config import config_options
+from .config import config_options
+from flask_bootstrap import Bootstrap
 
-bootstrap = Bootstrap()
+
 db = SQLAlchemy()
 
 def create_app(config_name):
@@ -14,7 +15,9 @@ def create_app(config_name):
     app.config.from_pyfile("config.py")
 
     # Initializing flask extensions
+    bootstrap = Bootstrap(app)
     bootstrap.init_app(app)
     db.init_app(app)
 
 from app import views
+from app import error
