a91ff283091caedfcc46a788c7f71db59e17000f
sheready <csstlw@gmail.com>
2020-07-18 19:11:35 +0300

    second commit

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from app import create_app,db
 from flask_script import Manager,Server
 from app.models import User, PitchCategory, Pitch, Comments, Votes
 from  flask_migrate import Migrate, MigrateCommand
-
+from flask_sqlalchemy import SQLAlchemy
 
 # Creating app instance
 app = create_app('production')
@@ -11,6 +11,7 @@ manager = Manager(app)
 manager.add_command('server',Server)
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
+db = SQLAlchemy(app)
 
 @manager.shell
 def make_shell_context():
