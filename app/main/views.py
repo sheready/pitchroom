b43b79ec7517d884fe775476ac4a2aedf0b3c1dd
sheready <csstlw@gmail.com>
2020-07-18 09:59:02 +0300

    add category route,categories route,add category route

--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,74 @@
-from flask import render_template
-from app import app
+from . import main
+from flask import render_template,request,redirect,url_for,abort
+from flask_login import login_required,current_user
+from ..models import User, PitchCategory, Pitch, Comments, Votes
+# from .forms import UpdateProfile
+from .forms import PitchForm, CommentForm, CategoryForm
+from .. import db
 
-# Views
-@app.route('/')
+#display categories on the landing page
+@main.route('/')
 def index():
+    """
+    View root page function that returns index page
+    """
 
-    '''
-    View root page function that returns the index page and its data
-    '''
+    all_category = PitchCategory.get_categories()
+    all_pitches = Pitch.query.order_by('id').all()
+    print(all_pitches)
 
-    title = 'Home - Welcome to Pitch Room'
-    return render_template('index.html', title = title)
+    title = 'Home- Welcome'
+    return render_template('index.html', title = title, categories=all_category, all_pitches=all_pitches)
+
+
+#Route for adding a new pitch
+@main.route('/category/new-pitch/<int:id>', methods=['GET', 'POST'])
+@login_required
+def new_pitch(id):
+    """
+    Function to check Pitches form and fetch data from the fields 
+    """
+    
+    form = PitchForm()
+    category = PitchCategory.query.filter_by(id=id).first()
+
+    if category is None:
+        abort(404)
+
+    if form.validate_on_submit():
+        content = form.content.data
+        new_pitch= Pitch(content=content, category_id = category.id, user_id = current_user.id)
+        new_pitch.save_pitch()
+        return redirect(url_for('.category', id=category.id))
+
+    return render_template('new_pitch.html', pitch_form=form, category=category)
+
+
+@main.route('/categories/<int:id>')
+def category(id):
+    category = PitchCategory.query.get(id)
+    if category is None:
+        abort(404)
+
+    pitches = Pitch.get_pitches(id)
+    return render_template('category.html', pitches=pitches, category=category)
+
+
+@main.route('/add/category', methods=['GET','POST'])
+@login_required
+def new_category():
+    """
+    View new group route function that returns a page with a form to create a category
+    """
+    
+    form = CategoryForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        new_category = PitchCategory(name = name)
+        new_category.save_category()
+
+        return redirect(url_for('.index'))
+
+    title = 'New category'
+
