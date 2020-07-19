2c6617721a8d1f59c2e9062504da8e0e157eaf5f
sheready <csstlw@gmail.com>
2020-07-19 18:02:55 +0300

    fix database errors

---
+++ migrations/versions/3aee2c097386_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 3aee2c097386
+Revises: e8d8bd5d5c4b
+Create Date: 2020-07-19 18:01:33.515204
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3aee2c097386'
+down_revision = 'e8d8bd5d5c4b'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ migrations/versions/e8d8bd5d5c4b_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: e8d8bd5d5c4b
+Revises: 53f2eb88ce4d
+Create Date: 2020-07-19 18:00:13.540335
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e8d8bd5d5c4b'
+down_revision = '53f2eb88ce4d'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
